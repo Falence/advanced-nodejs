880e26582ad5b9b02eb09ca8ca6d7e972d1bdf4a
Falence Lemungoh <falencelemungoh@gmail.com>
2021-11-30 16:57:37 +0100

    libuv os deligation

--- async.js
+++ async.js
@@ -2,11 +2,21 @@ const https = require('https');
 
 const start = Date.now();
 
-https
-  .request('https://google.com', res => {
-    res.on('data', () => {});
-    res.on('end', () => {
-      console.log(Date.now() - start);
+function doRequest() {
+  https
+    .request('https://google.com', res => {
+      res.on('data', () => {});
+      res.on('end', () => {
+        console.log(Date.now() - start);
+      })
     })
-  })
-  .end();
+    .end();
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
