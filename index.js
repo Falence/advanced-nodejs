671c81310133a247f67d7707c8105b9a2529bce2
Falence Lemungoh <falencelemungoh@gmail.com>
2021-12-01 18:34:14 +0100

    clutering

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+const cluster = require('cluster');
 const express = require('express');
 const app = express();
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start) {}
+// is the file being executed in master mode?
+if (cluster.isMaster) {
+  // cause index.js to be executed again but in child/slave mode
+  cluster.fork();
+} else {
+  // I'm a child, I'm going to act like the server and do nothing else
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(10000);
+    res.send('Hi there');
+  });
+
+  app.listen(5000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
-
-app.listen(3000);
