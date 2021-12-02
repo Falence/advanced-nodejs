463f4aa3d48dcf800fcf0935d5646d2874347c14
Falence Lemungoh <falencelemungoh@gmail.com>
2021-12-02 19:31:42 +0100

    creating a unique key

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,13 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
   console.log(`I'm about to run a query!`);
+
+  // Object.assign() - safely copy properties from one object to another
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  });
+
+  console.log(key);
+
   return exec.apply(this, arguments);
 }
